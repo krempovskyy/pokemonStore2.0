0c51978dd7207fd17582f9fcbf2cb561fc329ff0
Shanipiyadasa <shanipiyadasa@gmail.com>
2025-02-09 12:28:25 +0200

    global footer

---
+++ footer.php
@@ -0,0 +1,27 @@
+<!-- footer.php -->
+<footer>
+    <p>
+        Pokemon Store DAO <br>
+        <br>
+        +358 999 99 99 <br>
+        info@pokestore.lol <br>
+        11111, Hameenlinna
+    </p>
+    &copy; Madushani Piyadasa </p>
+</footer>
+<style>
+/*Footer styling*/
+footer{
+    font-family: 'Luckiest Guy', cursive;
+    text-align: center;
+    font-size: 15px;
+    color: #000000;
+    background-color: #c1c8e4;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    font-weight: lighter;
+}
+</style>
+
+</body>
+</html>
