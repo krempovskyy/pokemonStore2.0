2b858cefaff1b168204711e41f467212aa5a5d76
Madushani Piyadasa <shanipiyadasa@gmail.com>
2025-02-13 20:24:16 +0200

    contact basic page

---
+++ contact.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ContactUs</title>
+    <link rel="stylesheet" href="contactStyle.css">
+    <link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<?php
+// include the header file 
+$title ="Contact Page";
+//$md = "global header footer";
+include 'header.php';
+?>
+<>
+
+    <header>
+        <nav>
+            <ul class="nav">
+                <li class="nav-item"><a href="index.html" class="nav-link">Home</a></li>
+                <li class="nav-item"><a href="clothes.html" class="nav-link">Clothes</a></li>
+                <li class="nav-item"><a href="toys.html" class="nav-link">Toys</a></li>
+                <li class="nav-item"><a href="cart.html" class="nav-link">Cart</a></li>
+                <li class="nav-item"><a href="contact.html" class="nav-link">Contact Us</a></li> 
+            </ul>
+        </nav>
+        <h1 class="logo"><img src="images/logo.png" alt="logo"></h1>
+    </header>
+    <?php
+// include the footer file 
+
+include 'footer.php';
+?>
