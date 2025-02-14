076bb83342e1fd3c1ca34ce3e6d3fe72a99024a7
Madushani Piyadasa <shanipiyadasa@gmail.com>
2025-02-14 10:47:32 +0200

    connecting the style sheet

--- contact.php
+++ contact.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ContactUs</title>
-    <link rel="stylesheet" href="contactStyle.css">
     <link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
@@ -14,6 +13,9 @@ $title ="Contact Page";
 //$md = "global header footer";
 include 'includes/header.php';
 ?>
+
+<link rel="stylesheet" href="contactStyle.css">
+<link href="css/contactStyle.css" rel="stylesheet">
 
 <article>
         <div class="contact-section">
