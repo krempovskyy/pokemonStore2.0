90a99d507443d542c58c43a7baac9647539b2001
Madushani Piyadasa <shanipiyadasa@gmail.com>
2025-02-14 10:23:35 +0200

    contact page change

--- contact.php
+++ contact.php
@@ -16,18 +16,42 @@ include 'includes/header.php';
 ?>
 <>
 
-    <header>
-        <nav>
-            <ul class="nav">
-                <li class="nav-item"><a href="index.html" class="nav-link">Home</a></li>
-                <li class="nav-item"><a href="clothes.html" class="nav-link">Clothes</a></li>
-                <li class="nav-item"><a href="toys.html" class="nav-link">Toys</a></li>
-                <li class="nav-item"><a href="cart.html" class="nav-link">Cart</a></li>
-                <li class="nav-item"><a href="contact.html" class="nav-link">Contact Us</a></li> 
+<article>
+        <div class="contact-section">
+            <h2 class="custom-heading">We Love to hear from you !</h2>
+            <div class="contact-info">
+                <img src="images/contact.png" atl="contact" class="contact-image">
+                <p><strong> Pokemon Store</strong></p>
+                <p>+358 999 99 99</p>
+                <p>info@pokemon.lol</p>
+                <p>1111, Hämeenlinna</p>
+            </div>
+
+            <form action="#" method="POST" class="contact-form">
+                <div class="form-group">
+                    <label for="name">Name:</label>
+                    <input type="text" id="name" name="name" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">E-mail:</label>
+                    <input type="email" id="email" name="email" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="message">Message:</label>
+                    <textarea type="message" id="message" name="message" class="form-control" rows="4" required></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Send Message</button>
+            </form>
+        </div>
+
+        <div class="social-links">
+            <h3 class="custom-heading"> Follow Us</h3>
+            <ul>
+                <li class="nav-item1"><a href="https://www.pokemon.com/us" target="_blank">Main Website</a></li>
+                <li class="nav-item1"><a href="https://www.facebook.com/PokemonTCG/" target="_blank"> facebook</a></li>
             </ul>
-        </nav>
-        <h1 class="logo"><img src="images/logo.png" alt="logo"></h1>
-    </header>
+        </div>    
+    </article>
 
 <?php
 // include the footer file 
