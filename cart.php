18668a97930d26283de7364b1eecabc7be6181b5
Dmytro Krempovskyy <krempovskyydima@gmail.com>
2025-02-17 14:10:44 +0200

    cart page added + minor fixes

---
+++ cart.php
@@ -0,0 +1,13 @@
+<?php
+$title = "Pokemon Store - Your Cart";
+$md = "Check your added items before purchase";
+include 'includes/header.php';
+?>
+<link href="css/cartStyle.css" rel="stylesheet">
+
+<main>
+    
+           <p> Your cart is Empty !</p>
+</main>
+
+<?php include 'includes/footer.php'; ?>
